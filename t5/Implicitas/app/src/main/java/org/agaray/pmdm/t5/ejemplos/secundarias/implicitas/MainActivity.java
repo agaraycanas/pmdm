ea8f656890160368ec56de6eb6f6c91b91b41b7b
Alberto Garay (taller 13) <agaraycanas@educa.madrid.org>
2022-11-24 11:02:58 +0100

    actividades implicitas

--- t5/Implicitas/app/src/main/java/org/agaray/pmdm/t5/ejemplos/secundarias/implicitas/MainActivity.java
+++ t5/Implicitas/app/src/main/java/org/agaray/pmdm/t5/ejemplos/secundarias/implicitas/MainActivity.java
@@ -2,7 +2,10 @@ package org.agaray.pmdm.t5.ejemplos.secundarias.implicitas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void caso1(View v) {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_POWER_USAGE_SUMMARY);
+        startActivity(i);
+    }
+
+    public void caso2a(View v) {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("http://www.amazon.com"));
+        startActivity(i);
+    }
+
+    public void caso2b(View v) {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("sms:555443"));
+        startActivity(i);
+    }
+
+    public void caso3(View v) {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+
+        i.setType("vnd.android-dir/mms-sms");
+        i.putExtra("address", "5554433");
+        /*
+        i.setData(Uri.parse("sms:"));
+        */
+        startActivity(i);
+    }
+
+
+
 }
